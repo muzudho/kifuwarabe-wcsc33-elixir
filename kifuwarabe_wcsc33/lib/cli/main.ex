cb49f1d9457972b9eb49c8697cb6eb88748e512b
むずでょ <muzudho1@gmail.com>
2023-04-09 12:18:11 +0900

    Update main.ex

--- kifuwarabe_wcsc33/lib/cli/main.ex
+++ kifuwarabe_wcsc33/lib/cli/main.ex
@@ -6,14 +6,30 @@ defmodule KifuwarabeWcsc33.CLI do
     Logger.configure(level: :error)
 
     # 標準入力を受け取る |> 末尾の改行を削除するために trim() を使う
-    input = IO.gets("") |> String.trim()
+    input = IO.gets("") |> String.trim_trailing()
 
-    # Elixirに if～else-if～else 構造はない。 case文かcond文を使う
+    # コンピューター将棋の USIプロトコル（2007年1月17日原案公開。原案：トード・ロムスタッド。保守：将棋所）は、プロトコルとして基本が成ってないと言われることがあるが、
+    # そもそも オリジナルの UCIプロトコル（2000年11月原案公開。原案：ステファン・メイヤー＝カーレン）は もともと、仕様がバラバラで
+    # 対局するのも一苦労だったコンピューター・チェス・エンジン同士を、１つのマシンで対局させることができる初めての仕様であり、
+    # 2020年のWeb時代の感覚で見ると物足りないのは、みな分かって使っているので、納得して従うことにする
+    #
+    # USIの最新の動向は、やねうらおさんの MyShogi を参考にされたい
+    # https://github.com/yaneurao/MyShogi/blob/master/MyShogi/docs/USI2.0.md
+    #
+    # また、 USI はコンピューター・チェス由来であり、 WCSC（世界コンピューター将棋選手権）とは関りがない。
+    # WCSCではCSAプロトコルを使う。
+    # しかし、「将棋所」を使うと、USIプロトコルで書かれた将棋エンジンに代わって CSAプロトコルで通信してくれる
+    # http://shogidokoro.starfree.jp/
+    #
+    # Elixirに if～else-if～else 構造はない。 case文かcond文を使う。
     cond do
       input == "usi" ->
         IO.puts("id name Kifuwarabe")
         IO.puts("id author TAKAHASHI satoshi")
         IO.puts("usiok")
+
+      input == "isready" ->
+        IO.puts("readyok")
 
       # Otherwise
       true ->
