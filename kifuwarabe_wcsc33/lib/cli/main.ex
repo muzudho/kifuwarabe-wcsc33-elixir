bdb2a94f33c384776646fa7689eb2658b1946302
むずでょ <muzudho1@gmail.com>
2023-04-09 16:33:40 +0900

    usi loop

--- kifuwarabe_wcsc33/lib/cli/main.ex
+++ kifuwarabe_wcsc33/lib/cli/main.ex
@@ -1,25 +1,53 @@
 defmodule KifuwarabeWcsc33.CLI do
-  # このメソッドは、コンソール・アプリケーションのエントリー・ポイントではなく、
-  # 本来はスーパーバイザーの開始を書くメソッドだが、Elixirのスーパーバイザーが気に入らないので書かない。
+  @doc """
+  ## 雑談
+  
+      このメソッドは、コンソール・アプリケーションのエントリー・ポイントではなく、
+      本来はスーパーバイザーの開始を書くメソッドだが、Elixirのスーパーバイザーが気に入らないので書かない。
+  """
   def start(_type, _args) do
     # Elixirのロガーが気に入らないので、正常時には出ないようにする
     Logger.configure(level: :error)
 
+    # USIプロトコル対応
+    usi()
+
+    # 本来は、スーパーバイザーのPIDを返却する
+    {:ok, self()}
+  end
+
+  # Elixirに複数行コメントは無い。 関数の中では @doc、モジュールの中では @moduledoc を使う。マクロの中ではコメントは書けない
+  @moduledoc """
+  # if～else文の使い方
+  if input = "usi" do
+    IO.puts("id name Kifuwarabe")
+    IO.puts("id author TAKAHASHI satoshi")
+    IO.puts("usiok")
+  else
+    IO.puts("Hi! I am a Kifuwarabe. It's start! " <> input)
+  end
+  """
+
+  @doc """
+  ## 雑談
+  
+      コンピューター将棋の USIプロトコル（2007年1月17日原案公開。原案：トード・ロムスタッド。保守：将棋所）は、プロトコルとして基本が成ってないと言われることがあるが、
+      そもそも オリジナルの UCIプロトコル（2000年11月原案公開。原案：ステファン・メイヤー＝カーレン）は もともと、仕様がバラバラで
+      対局するのも一苦労だったコンピューター・チェス・エンジン同士を、１つのマシンで対局させることができる初めての仕様であり、
+      2020年のWeb時代の感覚で見ると物足りないのは、みな分かって使っているので、納得して従うことにする
+  
+      USIの最新の動向は、やねうらおさんの MyShogi を参考にされたい
+      https://github.com/yaneurao/MyShogi/blob/master/MyShogi/docs/USI2.0.md
+  
+      また、 USI はコンピューター・チェス由来であり、 WCSC（世界コンピューター将棋選手権）とは関りがない。
+      WCSCではCSAプロトコルを使う。
+      しかし、「将棋所」を使うと、USIプロトコルで書かれた将棋エンジンに代わって CSAプロトコルで通信してくれる
+      http://shogidokoro.starfree.jp/
+  """
+  defp usi() do
     # 標準入力を受け取る |> 末尾の改行を削除するために trim() を使う
     input = IO.gets("") |> String.trim_trailing()
 
-    # コンピューター将棋の USIプロトコル（2007年1月17日原案公開。原案：トード・ロムスタッド。保守：将棋所）は、プロトコルとして基本が成ってないと言われることがあるが、
-    # そもそも オリジナルの UCIプロトコル（2000年11月原案公開。原案：ステファン・メイヤー＝カーレン）は もともと、仕様がバラバラで
-    # 対局するのも一苦労だったコンピューター・チェス・エンジン同士を、１つのマシンで対局させることができる初めての仕様であり、
-    # 2020年のWeb時代の感覚で見ると物足りないのは、みな分かって使っているので、納得して従うことにする
-    #
-    # USIの最新の動向は、やねうらおさんの MyShogi を参考にされたい
-    # https://github.com/yaneurao/MyShogi/blob/master/MyShogi/docs/USI2.0.md
-    #
-    # また、 USI はコンピューター・チェス由来であり、 WCSC（世界コンピューター将棋選手権）とは関りがない。
-    # WCSCではCSAプロトコルを使う。
-    # しかし、「将棋所」を使うと、USIプロトコルで書かれた将棋エンジンに代わって CSAプロトコルで通信してくれる
-    # http://shogidokoro.starfree.jp/
     #
     # Elixirに if～else-if～else 構造はない。 case文かcond文を使う。
     cond do
@@ -59,8 +87,10 @@ defmodule KifuwarabeWcsc33.CLI do
       input == "quit" ->
         # > | quit  | (GUIから私へ) エンジン止めろ、アプリケーション終了しろ
         #   | 　　　 | (私からGUIへ送るものは何もありません)
+
+        # Elixirで 終了処理は、どう書けばいいのか？
+        # exit(0)
         System.stop()
-        nil
 
       # 以下は、USIプロトコルにないコマンド
       input == "board" ->
@@ -73,19 +103,7 @@ defmodule KifuwarabeWcsc33.CLI do
         IO.puts("Hi! I am a Kifuwarabe. It's start! " <> input)
     end
 
-    # 本来は、スーパーバイザーのPIDを返却する
-    {:ok, self()}
+    # 再帰ループ
+    usi()
   end
-
-  # Elixirに複数行コメントは無い。 関数の中では @doc、モジュールの中では @moduledoc を使う。マクロの中ではコメントは書けない
-  @moduledoc """
-  # if～else文の使い方
-  if input = "usi" do
-    IO.puts("id name Kifuwarabe")
-    IO.puts("id author TAKAHASHI satoshi")
-    IO.puts("usiok")
-  else
-    IO.puts("Hi! I am a Kifuwarabe. It's start! " <> input)
-  end
-  """
 end
