45ba2988e1f4fd9ed94f2095bb869c16ccc2b9bd
むずでょ <muzudho1@gmail.com>
2023-04-14 23:22:00 +0900

    make move

---
+++ kifuwarabe_wcsc33/lib/cli/mediators/to_turn.ex
@@ -0,0 +1,95 @@
+defmodule KifuwarabeWcsc33.CLI.Mediators.ToTurn do
+  @doc """
+    ピースを、ターンへ変換
+
+  ## Parameters
+
+    * `piece` - ピース（Piece；先後付きの駒種類）。スペースを渡すとエラー
+
+  ## Returns
+
+    0. ターン（Turn；先後）
+
+  """
+  def from_piece(piece) do
+    case piece do
+      #
+      # ▲せんて（Sente；先手） or したて（Shitate；下手）
+      # ============================================
+      #
+      # キング（King；玉）
+      :k1 -> :sente
+      # ルック（Rook；飛車）
+      :r1 -> :sente
+      # ビショップ（Bishop；角）
+      :b1 -> :sente
+      # ゴールド（Gold；金）
+      :g1 -> :sente
+      # シルバー（Silver；銀）
+      :s1 -> :sente
+      # ナイト（kNight；桂）
+      :n1 -> :sente
+      # ランス（Lance；香）
+      :l1 -> :sente
+      # ポーン（Pawn；歩）
+      :p1 -> :sente
+      # 玉は成れません
+      # :pk1
+      # It's reasonably a プロモーテッド・ルック（Promoted Rook；成飛）. It's actually ドラゴン（Dragon；竜）
+      :pr1 -> :sente
+      # It's reasonably a プロモーテッド・ビショップ（Promoted Bishop；成角）.  It's actually ホース（Horse；馬）. Ponanza calls ペガサス（Pegasus；天馬）
+      :pb1 -> :sente
+      # 金は成れません
+      # :pg1
+      # プロモーテッド・シルバー（Promoted Silver；成銀. Or 全 in one letter）
+      :ps1 -> :sente
+      # プロモーテッド・ナイト（Promoted kNight；成桂. Or 圭 in one letter）
+      :pn1 -> :sente
+      # プロモーテッド・ランス（Promoted Lance；成香. Or 杏 in one letter）
+      :pl1 -> :sente
+      # It's reasonably a プロモーテッド・ポーン（Promoted Pawn；成歩）. It's actually と（"To"；と is 金 cursive）
+      :pp1 -> :sente
+      #
+      # ▽ごて（Gote；後手） or うわて（Uwate；上手）
+      # =======================================
+      #
+      # キング（King；玉）
+      :k2 -> :gote
+      # ルック（Rook；飛車）
+      :r2 -> :gote
+      # ビショップ（Bishop；角）
+      :b2 -> :gote
+      # ゴールド（Gold；金）
+      :g2 -> :gote
+      # シルバー（Silver；銀）
+      :s2 -> :gote
+      # ナイト（kNight；桂）
+      :n2 -> :gote
+      # ランス（Lance；香）
+      :l2 -> :gote
+      # ポーン（Pawn；歩）
+      :p2 -> :gote
+      # 玉は成れません
+      # :pk2
+      # It's reasonably a プロモーテッド・ルック（Promoted Rook；成飛）. It's actually ドラゴン（Dragon；竜）
+      :pr2 -> :gote
+      # It's reasonably a プロモーテッド・ビショップ（Promoted Bishop；成角）.  It's actually ホース（Horse；馬）. Ponanza calls ペガサス（Pegasus；天馬）
+      :pb2 -> :gote
+      # 金は成れません
+      # :pg2
+      # プロモーテッド・シルバー（Promoted Silver；成銀. Or 全 in one letter）
+      :ps2 -> :gote
+      # プロモーテッド・ナイト（Promoted kNight；成桂. Or 圭 in one letter）
+      :pn2 -> :gote
+      # プロモーテッド・ランス（Promoted Lance；成香. Or 杏 in one letter）
+      :pl2 -> :gote
+      # It's reasonably a プロモーテッド・ポーン（Promoted Pawn；成歩）. It's actually と（"To"；と is 金 cursive）
+      :pp2 -> :gote
+      #
+      # それ以外はエラー
+      # ==============
+      #
+      _ -> raise "unexpected piece:#{piece}"
+    end
+  end
+end
--- kifuwarabe_wcsc33/lib/cli/routes/move_generation.ex
+++ kifuwarabe_wcsc33/lib/cli/routes/move_generation.ex
@@ -14,8 +14,13 @@ defmodule KifuwarabeWcsc33.CLI.Routes.MoveGeneration do
     move_list = []
 
     # TODO 盤上の自分の駒と、持っている駒の数だけ、合法手が生成できる
-    # nil は除去
-    move_sub_list = Enum.map(pos.board, fn {sq,piece} -> pos|>make_move_list_by_piece(sq,piece) end) |> Enum.filter(& !is_nil(&1))
+    # |> スペース（:sp；空マス）は除去
+    # |> マス番地と先後付きの駒種類から、指し手生成
+    # |> 指し手が nil なら除去
+    move_sub_list = pos.board
+      |> Enum.filter(fn{_sq,piece} -> piece != :sp end)
+      |> Enum.map(fn {sq,piece} -> pos|>make_move_list_by_piece(sq,piece) end)
+      |> Enum.filter(& !is_nil(&1))
 
     move_list = move_list ++ move_sub_list
 
@@ -31,7 +36,9 @@ defmodule KifuwarabeWcsc33.CLI.Routes.MoveGeneration do
   #   * `piece` - ピース（Piece；先後付きの駒種類）
   #
   defp make_move_list_by_piece(pos, sq, piece) do
-    IO.puts("turn:#{pos.turn} sq:#{sq} piece:#{piece}")
+    # IO.puts("pos.turn:#{pos.turn} sq:#{sq} piece:#{piece}")
+    turn_of_piece = KifuwarabeWcsc33.CLI.Mediators.ToTurn.from_piece(piece)
+    IO.puts("pos.turn:#{pos.turn} sq:#{sq} piece:#{piece} turn_of_piece:#{turn_of_piece}")
     nil
   end
 end
