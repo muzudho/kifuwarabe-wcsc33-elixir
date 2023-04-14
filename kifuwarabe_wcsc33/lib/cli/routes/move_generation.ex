0dec8811d2d1371f8afcc4f54dfd831b0053f2e1
むずでょ <muzudho1@gmail.com>
2023-04-14 22:57:37 +0900

    make move list

--- kifuwarabe_wcsc33/lib/cli/routes/move_generation.ex
+++ kifuwarabe_wcsc33/lib/cli/routes/move_generation.ex
@@ -15,12 +15,23 @@ defmodule KifuwarabeWcsc33.CLI.Routes.MoveGeneration do
 
     # TODO 盤上の自分の駒と、持っている駒の数だけ、合法手が生成できる
     # nil は除去
-    move_sub_list = Enum.map(pos.board, fn _piece -> nil end) |> Enum.filter(& !is_nil(&1))
+    move_sub_list = Enum.map(pos.board, fn {sq,piece} -> pos|>make_move_list_by_piece(sq,piece) end) |> Enum.filter(& !is_nil(&1))
 
     move_list = move_list ++ move_sub_list
 
     # move_list = move_list ++ [KifuwarabeWcsc33.CLI.Models.Move.new()]
 
     move_list
+  end
+
+  # ## 引数
+  #
+  #   * `pos` - ポジション（Position；局面）
+  #   * `sq` - スクウェア（Square：マス番地）
+  #   * `piece` - ピース（Piece；先後付きの駒種類）
+  #
+  defp make_move_list_by_piece(pos, sq, piece) do
+    IO.puts("turn:#{pos.turn} sq:#{sq} piece:#{piece}")
+    nil
   end
 end
