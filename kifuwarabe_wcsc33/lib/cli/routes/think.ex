792acc36f8c60ddc81db69fb7f3833c818f96a3d
むずでょ <muzudho1@gmail.com>
2023-04-20 00:56:30 +0900

    Update think.ex

--- kifuwarabe_wcsc33/lib/cli/routes/think.ex
+++ kifuwarabe_wcsc33/lib/cli/routes/think.ex
@@ -65,53 +65,44 @@ defmodule KifuwarabeWcsc33.CLI.Routes.Think do
         {move_list |> List.delete_at(0), best_move}
       end
 
-    pos =
-      if best_move.destination == nil do
-        # 投了なら、おわり
-        pos
+    if best_move.destination == nil do
+      # 投了なら、おわり
+      {pos, move_list, best_move}
 
+    else
+      # とりあえず、指してみる
+      best_move_code = KifuwarabeWcsc33.CLI.Views.Move.as_code(best_move)
+      IO.puts("[think choice] best_move:#{best_move_code}")
+      pos = pos |> KifuwarabeWcsc33.CLI.Routes.DoMove.move(best_move)
+      # 手番がひっくり返ったことに注意
+      IO.puts(KifuwarabeWcsc33.CLI.Views.Position.stringify(pos))
+
+      # 一手指したあとの、自玉の位置を検索（ここでは相手番なので、さっきの手番は逆側）
+      king_sq = pos |> KifuwarabeWcsc33.CLI.Finder.Square.find_king_on_board(pos.opponent_turn)
+      IO.puts("[think choice] king_sq:#{king_sq}")
+
+      if king_sq != nil do
+
+        # TODO 自分から相手の利きへ飛び込む手（自殺手）は除外したい
+        # （ここでは相手番なので、さっきの手番は逆側）
+        if pos |> KifuwarabeWcsc33.CLI.Thesis.IsSuicideMove.is_suicide_move?(king_sq) do
+          # 自殺手だ
+          # 戻す
+          IO.puts("[think choice] #{best_move_code} is suicide move. Undo move")
+          pos = pos |> KifuwarabeWcsc33.CLI.Routes.UndoMove.move()
+
+          # Recursive
+          # =========
+          #
+          # - ベストムーブか、投了のどちらかを取得するまで続ける
+          pos |> choice(move_list)
+        else
+          IO.puts("[think choice] #{best_move_code} is no suicide move. Ok")
+          {pos, move_list, best_move}
+        end
       else
-        # とりあえず、指してみる
-        best_move_code = KifuwarabeWcsc33.CLI.Views.Move.as_code(best_move)
-        IO.puts("[think choice] best_move:#{best_move_code}")
-        pos = pos |> KifuwarabeWcsc33.CLI.Routes.DoMove.move(best_move)
-        # 手番がひっくり返ったことに注意
-        IO.puts(KifuwarabeWcsc33.CLI.Views.Position.stringify(pos))
-
-        # 一手指したあとの、自玉の位置を検索（ここでは相手番なので、さっきの手番は逆側）
-        king_sq = pos |> KifuwarabeWcsc33.CLI.Finder.Square.find_king_on_board(pos.opponent_turn)
-        IO.puts("[think choice] king_sq:#{king_sq}")
-
-        pos =
-          if king_sq != nil do
-
-            # TODO 自分から相手の利きへ飛び込む手（自殺手）は除外したい
-            pos =
-              # （ここでは相手番なので、さっきの手番は逆側）
-              if pos |> KifuwarabeWcsc33.CLI.Thesis.IsSuicideMove.is_suicide_move?(king_sq) do
-                # 自殺手だ
-                # 戻す
-                IO.puts("[think choice] #{best_move_code} is suicide move. Undo move")
-                pos = pos |> KifuwarabeWcsc33.CLI.Routes.UndoMove.move()
-
-                # Recursive
-                # =========
-                #
-                # - ベストムーブか、投了のどちらかを取得するまで続ける
-                pos |> choice(move_list)
-                pos
-              else
-                IO.puts("[think choice] #{best_move_code} is no suicide move. Ok")
-                pos
-              end
-            pos
-          else
-            pos
-          end
-
-        pos
+        {pos, move_list, best_move}
       end
-
-    {pos, move_list, best_move}
+    end
   end
 end
